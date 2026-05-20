d15487cda912b1e082ae22392e0db136aaf15eaa
Mert Dundar <mert.dundar@hotmail.com>
2026-05-20 07:18:57 +0300

    Simulation page action buttons

---
+++ resources/js/components/Controls.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="controls">
+        <AppButton
+            :disabled="!store.canPlayAll"
+            @click="store.playAll()"
+        >
+            Play All Weeks
+        </AppButton>
+        <AppButton
+            :disabled="!store.canPlayNext"
+            @click="store.playNext()"
+        >
+            Play Next Week
+        </AppButton>
+        <AppButton
+            variant="danger"
+            :disabled="store.loading"
+            @click="store.reset()"
+        >
+            Reset Data
+        </AppButton>
+    </div>
+</template>
+
+<script setup>
+import AppButton from './AppButton.vue';
+import { useLeagueStore } from '../stores/league.js';
+
+const store = useLeagueStore();
+</script>
+
+<style scoped>
+.controls {
+    display: flex;
+    justify-content: space-around;
+    gap: 16px;
+    margin-top: 24px;
+    flex-wrap: wrap;
+}
+</style>
